e088586f01a4b03a4b13262f45f835c0a82095b7
Kikomali <kikomali2517@gmail.com>
2023-03-02 20:39:32 +0100

    Updated algorithm

--- Program/Algorithm.cpp
+++ Program/Algorithm.cpp
@@ -1,10 +1,56 @@
 #include "Algorithm.h"
 
-Algorithm::Algorithm(){}
+Algorithm::Algorithm(){
+  SetMowerOverlap(0.8);
+}
+
 Algorithm::~Algorithm(){}
 
-int Algorithm::AddOutline(List<Point> outline){
+void Algorithm::SetMowerDiameter(double mowerDiameter){
+  this -> mowerDiameter = mowerDiameter;
+}
+
+void Algorithm::SetMowerOverlap(double mowerOverlap){
+  this -> mowerOverlap = mowerOverlap;
+}
+
+void Algorithm::AddOutline(List<Point> outline){
   outlines.add(outline);
+}
+
+void Algorithm::FindBoundaries(){
+  //Find outline boundaries
+  maxX = -999999;
+  maxY = -999999;
+
+  minX = 999999;
+  minY = 999999;
+
+  for (int i = 0; i < outlines[0].getSize(); i++){
+    Point p = outlines[0][i];
+
+    if (p.x() > maxX){
+      maxX = p.x();
+    }
+    if (p.x() < minX){
+      minX = p.x();
+    }
+
+    if (p.y() > maxY){
+      maxY = p.y();
+    }
+    if (p.y() < minY){
+      minY = p.y();
+    }
+  }
+}
+
+void Algorithm::CreateInfill(){
+  //Find outline boundaries
+  FindBoundaries();
+
+  //Divide with lines that are parallel to the X axis
+  int numOfLines = ceil((maxY - minY) / (mowerDiameter * mowerOverlap));
 }
 
 String Algorithm::GenerateGCODE(){
--- Program/Algorithm.h
+++ Program/Algorithm.h
@@ -11,11 +11,31 @@ class Algorithm {
 public:
   Algorithm();
   ~Algorithm();
-  int AddOutline(List<Point>);
+
+  //Settings
+  void SetMowerDiameter(double);
+  void SetMowerOverlap(double);
+
+  //Setup
+  void AddOutline(List<Point>);
+
+  //Functions
   String GenerateGCODE();
+    void CreateInfill();
+      void FindBoundaries();
 private:
+  //SETTINGS
+  double mowerDiameter; //Diameter of the cutting surface, in meters
+  double mowerOverlap; //Percentage of overlap when generating GCODE, initially set to 80%
+
   List<List<Point>> outlines;
+  List<List<Point>> connectedOutlines;
+
   String GCODE;
+
+  //Outline boundaries
+  double minX, maxX;
+  double minY, maxY;
 };
  
 #endif
